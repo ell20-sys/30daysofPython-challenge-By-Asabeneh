6292243e7f137b3982871a8fb8671794b9c5ea4e
ell20-sys <71948227+ell20-sys@users.noreply.github.com>
2023-03-09 09:08:10 +0000

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,47 @@
+# Day 2: 30 days of python programming
+#assigning variables
+first_name = 'Elliot'
+last_name = 'Awe'
+full_name = 'Elliot Awe'
+country = 'Ghana'
+city = 'Accra'
+age = 20
+year = 2002
+is_married = False
+is_true= True
+is_light_on = False
+# first, last, full, countr, cit, ag, is_mar, is_tr, is_l_on = 'Elliot','Awe','Elliot Awe', 'Ghana', 'Accra', 20, 2002, False, True, False
+
+#checking types
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+# print(type(first, last, full, countr, cit, ag, is_mar, is_tr, is_l_on))
+
+#length of my first name
+print(len(first_name))
+
+
+num_one = 5
+num_two = 4
+total = num_one + num_two
+variable_diff = num_one - num_two
+variable_product = num_one * num_two
+variable_division = num_one / num_two
+
+radius = 30
+pi = 3.142
+area_of_circle = 2 * pi * radius
+circum_of_circle = pi * (radius)**2
+radius = input("Enter the Radius: ")
+print("Raidus = ", area_of_circle)
+
+
+
